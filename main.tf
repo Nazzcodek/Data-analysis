74f0249713f4ea6d84eb5ac0828f7688e7c49032
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2023-01-08 15:39:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Configure the AWS provider
 provider "aws" {
   region = "eu-central-1"
@@ -29,35 +28,3 @@ resource "aws_lambda_function" "export_function" {
     }
   }
 }
-=======
-# Configure the AWS provider
-provider "aws" {
-  region = "eu-central-1"
-}
-
-# zipping the export.py python file
-data "archive_file" "zip_the_exportpy_code" {
-type        = "zip"
-source_file = "${path.module}/export.py" 
-output_path = "export.zip"
-}
-
-# Create a Lambda function to run the export script
-resource "aws_lambda_function" "export_function" {
-  filename         = "export.zip" 
-  function_name    = "export_table_to_s3"
-  role             = "arn:aws:iam::711328479423:role/lambda_export_role"
-  handler          = "export.export_table_to_s3"
-  runtime          = "python3.9"
-
-    environment {
-    variables = {
-      "RDS_HOST"     = "d2b-internal-assessment-dwh.cxeuj0ektqdz.eu-central-1.rds.amazonaws.com",
-      "RDS_USER"     = "nasibell8682",
-      "RDS_PASSWORD" = "kBVtydZRpe",
-      "RDS_DATABASE" = "d2b_assessment",
-      "S3_BUCKET"    = "d2b-internal-assessment-bucket"
-    }
-  }
-}
->>>>>>> e037781cebf45f606383bad43a6ec89ede754543
